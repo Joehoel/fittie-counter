953736e435da4e360567a98181713e9b2775ef3a
Joël Kuijper <jmkuijper@outlook.com>
2024-11-07 22:07:33 +0100

    feat: add umami

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -3,6 +3,8 @@
     <head>
         <meta charset="utf-8">
 
+        <script defer src="https://analytics.longux.xyz/script.js" data-website-id="0cc4b95e-0fee-4ef8-8747-707b7aed50c1"></script>
+
         <meta name="application-name" content="{{ config('app.name') }}">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="viewport" content="width=device-width, initial-scale=1">
